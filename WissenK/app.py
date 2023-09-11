e67e7bbf0528a824c5969de3b28b6d767abd7fc6
AbhiramPollur <138471216+AbhiramPollur@users.noreply.github.com>
2023-09-11 13:44:03 +0530

    Add files via upload

--- WissenK/app.py
+++ WissenK/app.py
@@ -6,8 +6,7 @@ from sklearn.metrics import classification_report
 
 app = Flask(__name__)
 
-# Load the trained model
-model_path = 'models/knn_model.pkl'
+model_path = 'models/KNC.pkl'
 if os.path.exists(model_path):
     with open(model_path, 'rb') as model_file:
         knn_classifier = pickle.load(model_file)
@@ -25,7 +24,6 @@ def predict():
     if knn_classifier is None:
         return "Model not found. Please train the model first."
 
-    # Get user input
     if 'csv_file' in request.files:
         csv_file = request.files['csv_file']
         if csv_file.filename != '':
@@ -36,17 +34,52 @@ def predict():
             return render_template('result.html', report=report)
 
     else:
-        time = float(request.form['time'])
-        thighx = float(request.form['thighx'])
-        thighy = float(request.form['thighy'])
-        thighz = float(request.form['thighz'])
-        backx = float(request.form['backx'])
-        backy = float(request.form['backy'])
-        backz = float(request.form['backz'])
+        time = float(request.form['timestamp'])
+        thighx = float(request.form['thigh_x'])
+        thighy = float(request.form['thigh_y'])
+        thighz = float(request.form['thigh_z'])
+        backx = float(request.form['back_x'])
+        backy = float(request.form['back_y'])
+        backz = float(request.form['back_z'])
 
         input_data = [[time, thighx, thighy, thighz, backx, backy, backz]]
         prediction = knn_classifier.predict(input_data)  # Replace 'input_data' with your preprocessed data
         return render_template('result.html', prediction=prediction)
+
+
+@app.route('/retrain', methods=['GET', 'POST'])
+def retrain():
+    global knn_classifier
+
+    if request.method == 'POST':
+        new_training_data = request.files['new_training_data']
+
+        if new_training_data:
+            temp_path = 'temp_new_training_data.csv'
+            new_training_data.save(temp_path)
+
+            df_new_data = pd.read_csv(temp_path)
+
+            X_new = df_new_data.drop('label', axis=1)
+            X_new['timestamp'] = pd.to_datetime(X_new['timestamp']).astype('int64') // 10**9
+            y_new = df_new_data[['labels']]
+
+            knn_classifier.fit(X_new, y_new)
+
+            new_model_path = 'models/new_KNC_model.pkl'
+            with open(new_model_path, 'wb') as new_model_file:
+                pickle.dump(knn_classifier, new_model_file)
+
+            if os.path.exists(new_model_path):
+                os.remove(model_path)
+                os.rename(new_model_path, model_path)
+
+            if os.path.exists(temp_path):
+                os.remove(temp_path)
+
+            return "Model retrained successfully."
+
+    return render_template('retrain.html')
 
 
 if __name__ == '__main__':
